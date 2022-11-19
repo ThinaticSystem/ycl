606ace3ce1f7985c167301ea820938bf788678e2
ThinaticSystem <git@thinaticsystem.com>
2022-11-19 19:33:57 +0900

    setup Standalone Applicationに

--- apps/ycl/src/app/app.component.ts
+++ apps/ycl/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ycl-root',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { }
+export class AppComponent {}
--- apps/ycl/src/main.ts
+++ apps/ycl/src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
+const routes: Routes = [
+  { path: 'futomomo', loadComponent: () => import('./app/pages/futomomo/futomomo.component') },
+];
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
