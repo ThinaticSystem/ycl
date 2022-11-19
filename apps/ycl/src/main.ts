3032614ce60c1f627318e8e7a77ff37bac55d555
ThinaticSystem <git@thinaticsystem.com>
2022-11-19 23:41:15 +0900

    dependency Angular Material
    nx g時にjest-preset-angularをpackage.jsonから省く裏技使用

--- apps/ycl/src/main.ts
+++ apps/ycl/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 const routes: Routes = [
   { path: 'futomomo', loadComponent: () => import('./app/pages/futomomo/futomomo.component') },
@@ -9,5 +11,5 @@ const routes: Routes = [
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
