210c7e90df98bc9ccf080cd1f080fdf1dfdf6946
ThinaticSystem <git@thinaticsystem.com>
2022-11-23 17:21:26 +0900

    コンボボックスでPart追加

--- apps/ycl/src/app/pages/futomomo/futomomo.component.ts
+++ apps/ycl/src/app/pages/futomomo/futomomo.component.ts
@@ -1,28 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import TimelineComponent from '../../parts/timeline/timeline.component';
 import PosterComponent from '../../parts/poster/poster.component';
-import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop'
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'ycl-futomomo',
   standalone: true,
   templateUrl: './futomomo.component.html',
   styleUrls: ['./futomomo.component.scss'],
-  imports: [CommonModule, DragDropModule, TimelineComponent, PosterComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    TimelineComponent,
+    PosterComponent,
+  ],
 })
-export default class FutomomoComponent {
-  public appearParts: ('timeline' | 'poster')[] = [];
+export default class FutomomoComponent implements OnInit, OnDestroy {
+  private onDestroy$ = new Subject<null>();
+  readonly PartsList = ['timeline', 'poster'] as const;
 
-  public addPart(partType: 'timeline' | 'poster') {
-    this.appearParts.push(partType);
+  public appearParts: typeof this.PartsList[number][] = [];
+  public editMode = false;
+  public f_newPart = new FormControl<typeof this.PartsList[number] | null>(null);
+
+  ngOnInit() {
+    this.f_newPart.valueChanges
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((value) => {
+        if (value === null) {
+          return;
+        }
+        this.addPart(value);
+        this.f_newPart.reset();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(null);
+    this.onDestroy$.complete();
+  }
+
+  public addPart(part: typeof this.PartsList[number]) {
+    if (this.f_newPart.value === null) {
+      return;
+    }
+    this.appearParts.push(part);
   }
 
   public removePart(index: number) {
     this.appearParts = this.appearParts.filter((_v, i) => i != index);
-  };
+  }
 
   public onDrop(event: CdkDragDrop<typeof this.appearParts>) {
-    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    moveItemInArray(
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 }
