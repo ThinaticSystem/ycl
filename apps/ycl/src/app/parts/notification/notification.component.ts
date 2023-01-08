e7b5ec5a6938e245f5c45854449b1bd1e1735fd8
ThinaticSystem <git@thinaticsystem.com>
2023-01-08 19:00:03 +0900

    add Notification component

---
+++ apps/ycl/src/app/parts/notification/notification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ycl-notification',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss'],
+})
+export class NotificationComponent {}
